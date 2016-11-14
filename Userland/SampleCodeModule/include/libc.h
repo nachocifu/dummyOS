3dfc8c45b4d88b94ded20365231c988bea1953ce
Ignacio Cifuentes <ignacio.cifuentes948@gmail.com>
2016-11-14 15:43:45 -0300

    Algo de avance con el params interpreter en la shell>

--- Userland/SampleCodeModule/include/libc.h
+++ Userland/SampleCodeModule/include/libc.h
@@ -2,5 +2,11 @@
 #include <stdint.h>
 #include <stdarg.h>
 
+#define TRUE 1
+#define FALSE 0
+#define MAX_PARAMS_SHELL 5
+
+
 void printf(char *format, ...);
 void scanf(char *format, ...);
+char** str_split(char *str);
--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -1,21 +1,61 @@
 /* sampleCodeModule.c */
 #include <libc.h>
 
+void initWelcome ();
 char * v = (char*)0xB8000;
 
 static int var1 = 0;
 static int var2 = 0;
-
+static int exit = FALSE;
 static int k = 0;
 
 int main() {
-	//All the following code may be removed 
+	
+	// Welcomeing sign
+	initWelcome();
 
-	printf("Empezo userland\n");
+	// Method containing tests. 
+	// To be used instead of commenting o erasing code in main.
+	// Prevents git merge conflicts
+	// testingMethod();
+	
+	// Loop shell until user exits OS
+	while(!exit){
+		char f[100];
+		printf("yourName@dummyOS $ ");
+		scanf("%s", &f);
+		printf("El comando ingresado fue: %s",f);
+		char** params = str_split(f);
+		
+		parseCommand(params);
+	}
 
+
+ 
+	//Test if BSS is properly set up
+	if (var1 == 0 && var2 == 0)
+		return 0xDEADC0DE;
+
+	return 0xDEADBEEF;
+}
+
+void parseCommand(char** params) {
+	printf("Hola Mundo!\n");
+	printf("PARAM = %s\n", &params[0]);
+	printf("PARAM = %s\n", &params[1]);
+	printf("PARAM = %s\n", &params[2]);
+	printf("PARAM = %s\n", &params[3]);
+	printf("PARAM = %s\n", &params[4]);
+
+}
+
+void initWelcome () {
+	printf("Welcome To DummyOS\n\n");
+	return;
+}
+
+void testingMethod() {
 	char f[10];
-
-
 	int valor1 = 0, valor2 = 0;
 	
 	while(1){
@@ -33,19 +73,4 @@ int main() {
 
 	
 	}
-	
-	while(1){
-		k++;
-		if (k > 10000000){
-			k = 0;
-		}
-	}
-
-
- 
-	//Test if BSS is properly set up
-	if (var1 == 0 && var2 == 0)
-		return 0xDEADC0DE;
-
-	return 0xDEADBEEF;
 }
