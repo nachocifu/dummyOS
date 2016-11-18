da63ceabeaeb92040fb881be129c80264f78cf4a
Ignacio Cifuentes <ignacio.cifuentes948@gmail.com>
2016-11-18 14:14:15 -0300

    welcome screen

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -85,30 +85,8 @@ void printManPage (char *command) {
 }
 
 void initWelcome () {
-	printf("Welcome To DummyOS\n\n");
+	printf(
+		"    *****************************************\n*                                       *\n*        Welcome To DummyOS             *\n*                                       *\n*****************************************\n"
+		);
 	return;
 }
-
-void testingMethod() {
-	char f[10];
-	int valor1 = 0, valor2 = 0;
-	
-	while(1){
-		if (valor1 == 0){
-			printf("%s", "Por favor ingrese su primer numero. Este numero sera sumado al que proximamente sera solicitado en este pequeno formulario virtual (por favorrrrrrr): ");
-			scanf("%d", &valor1);
-		}
-		if (valor2 == 0){
-			printf("%s", "Ingrese el segundo: ");
-			scanf("%d", &valor2);
-		}
-		printf("Su suma es: %d\n", valor1+valor2);
-		valor1 = 0;
-		valor2 = 0;
-
-	
-	}
-}
-
-
-
