ccab56eb7c071d555e1456af3196b6e81a7b5e4b
Ignacio Cifuentes <ignacio.cifuentes948@gmail.com>
2016-11-21 16:40:06 -0300

    Clean up main userland

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -3,22 +3,12 @@
 #include <commands.h>
 
 void initWelcome ();
-char * v = (char*)0xB8000;
-
-static int var1 = 0;
-static int var2 = 0;
 static int exit = FALSE;
-static int k = 0;
 
 int main() {
 	
-	// Welcomeing sign
+	// OS welcome
 	initWelcome();
-
-	// Method containing tests. 
-	// To be used instead of commenting o erasing code in main.
-	// Prevents git merge conflicts
-	// testingMethod();
 	
 	// Loop shell until user exits OS
 	while(!exit){
@@ -29,15 +19,12 @@ int main() {
 		exit = runCommand(parseCommand(params[0]), params);
 	}
 
-
- 
-	//Test if BSS is properly set up
-	if (var1 == 0 && var2 == 0)
-		return 0xDEADC0DE;
-
-	return 0xDEADBEEF;
+	return 0;
 }
 
+/**
+Welcoming sign of the OS
+*/
 void initWelcome () {
 	printf(
 		"    *****************************************\n*                                       *\n*        Welcome To DummyOS             *\n*                                       *\n*****************************************\n"
